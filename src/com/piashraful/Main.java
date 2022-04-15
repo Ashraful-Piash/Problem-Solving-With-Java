f74ea0497ca3f65a55d26f3bfc5f6a56ef5018b7
Ashraful Piash <piashrafulhaque@gmail.com>
2022-04-16 02:23:33 +0600

    modified

--- src/com/piashraful/Main.java
+++ src/com/piashraful/Main.java
@@ -3,7 +3,8 @@ package com.piashraful;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("hello");;
+        System.out.println("hello");
+        System.out.println("hi");
 	// write your code here
     }
 }
