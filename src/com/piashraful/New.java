2ecf06b52fa46f76c94a56ecf1c1c9bb7d5c424e
Ashraful Piash <piashrafulhaque@gmail.com>
2022-04-17 12:14:49 +0600

    modified

--- src/com/piashraful/New.java
+++ src/com/piashraful/New.java
@@ -4,5 +4,6 @@ public class New {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("Alhamdulillah");
+        System.out.println("hello");
     }
 }
